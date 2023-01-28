6c393210c2a65d48bc46655a266f148a0f9ce4ee
Alexsus <alexsuslin@inbox.ru>
2023-01-28 17:06:51 +0300

    create test

---
+++ src/04/04.test.ts
@@ -0,0 +1,19 @@
+export const usersObj={
+    '0':'Alex',
+    '1':'Ser',
+    '2':'Dima',
+    '3':'Anna',
+}
+
+export const users={
+    '101':{id:101, name:'Alex'},
+    '32323':{id:32323, name:'Ser'},
+    '1212':{id:1212, name:'Dima'},
+    '123':{id:123, name:'Anna'}
+}
+export const usersArray=[
+    {id:101, name:'Alex'},
+    {id:32323, name:'Ser'},
+    {id:1212, name:'Dima'},
+    {id:123, name:'Anna'}
+]
--- src/04/04.ts
+++ src/04/04.ts
@@ -1,1 +1,9 @@
-//
+import {usersObj} from "./04.test";
+
+
+test('should select corresponding user from obj', ()=>{
+    expect(usersObj[0]).toBe('Alex')
+    expect(usersObj[1]).toBe('Ser')
+    expect(usersObj[2]).toBe('Dima')
+    expect(usersObj[3]).toBe('Anna')
+})
